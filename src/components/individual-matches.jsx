ca8bd4af6e41afcc12b37396bee1c8557822295e
Elton Kamami <4094917+pixelgrid@users.noreply.github.com>
2026-01-23 18:44:16 +0000

    show selected player from individual matches

--- src/components/individual-matches.jsx
+++ src/components/individual-matches.jsx
@@ -1,13 +1,15 @@
+import React from 'react'
 import DisciplineImage from '../components/discipline-image.jsx'
 
 export default function IndividualMatches({matches}){
-  return <div className="matchesoverview">{matches.map(match => {
-    return <div className={`individualMatch ${match.status} winner-${match.winner} match-${match.matchId}`}>
+  const [selectedPlayer, setSelectedPlayer] = React.useState(null);
+  return <div className="matchesoverview">{matches.map((match, index) => {
+    return <div key={index} className={`individualMatch ${match.status} winner-${match.winner} match-${match.matchId}`}>
       <DisciplineImage discipline={match.discipline} />
       <span>RT{match.raceTo}</span>
-      <span className="player A">{match.playerA}</span>
+      <span className={`player A ${selectedPlayer === match.playerA ? 'selectedPlayer' : ''}`} onClick={() => setSelectedPlayer(match.playerA)}>{match.playerA}</span>
       <span className="score"><span className="scoreA">{match.scoreA}</span> - <span className="scoreB">{match.scoreB}</span></span>
-      <span className="player B">{match.playerB}</span>
+      <span className={`player B ${selectedPlayer === match.playerB ? 'selectedPlayer' : ''}`} onClick={() => setSelectedPlayer(match.playerB)}>{match.playerB}</span>
     </div>
   })}</div>
 }
